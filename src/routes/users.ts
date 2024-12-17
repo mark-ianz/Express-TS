d39b69615a94f90a3ed2ed2cd21484c2c4fb344d
mark-ianz <bustillomarkian23@gmail.com>
2024-12-17 13:50:56 +0800

    renamed function

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createUser, get_student_users } from "../controllers/users";
+import { create_user, get_student_users } from "../controllers/users";
 
 const router = Router();
 
 router.get("/", get_student_users);
 router.get ("/:id", get_student_users);
-router.post ("/", createUser);
+router.post ("/", create_user);
 
 export default router;

1ff2eb20f18353d3fdc6a7313130bd8896fc6533
mark-ianz <bustillomarkian23@gmail.com>
2024-12-17 11:49:59 +0800

    created a /:id route to get single user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,6 +4,7 @@ import { createUser, get_student_users } from "../controllers/users";
 const router = Router();
 
 router.get("/", get_student_users);
+router.get ("/:id", get_student_users);
 router.post ("/", createUser);
 
 export default router;

462bf55f988c4388f825cd0b21f845793067aad2
mark-ianz <bustillomarkian23@gmail.com>
2024-12-17 13:50:44 +0800

    adjusted the return statement for get_student_users and fixed the create_user fn

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { CreateUser } from "../dtos/CreateUser.dto";
 import { pool } from "../database";
+import { User } from "../types/user.types";
+import { ResultSetHeader } from "mysql2";
 
 const user_select_statement = "id, username, email, access";
 
@@ -9,23 +11,39 @@ export async function get_student_users(
   res: Response
 ) {
   const { id } = req.params;
-  const [rows] = await pool.query(
-    id
-      ? `SELECT ${user_select_statement} FROM users WHERE id = ${id}`
-      : `SELECT ${user_select_statement} FROM users`
-  );
-  res.json({
-    users: rows,
-  });
+
+  const query = id
+    ? `SELECT ${user_select_statement} FROM users WHERE id = ${id}`
+    : `SELECT ${user_select_statement} FROM users`;
+  const params = id && [id];
+
+  const [rows] = await pool.query<User[]>(query, params);
+
+  if (rows.length === 0) {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  res.json(id ? rows[0] : { users: rows });
 }
 
-export function createUser(req: Request<{}, {}, CreateUser>, res: Response) {
-  const { username, password } = req.body;
-  console.log(req.query);
+export async function create_user(
+  req: Request<{}, {}, CreateUser>,
+  res: Response
+) {
+  const { username, password, email, access } = req.body;
+  const params = [username, password, email, access];
+  const query = `INSERT INTO users 
+  (username, password, email, access) 
+  VALUES (?, ?, ?, ?)`;
+
+  const [result] = await pool.query<ResultSetHeader>(query, params);
 
   res.json({
+    id: result.insertId,
     username,
-    password,
-    message: "User created",
+    email,
+    access,
   });
 }
